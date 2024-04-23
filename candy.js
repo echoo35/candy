7ea575c7f63fdf483cca433a18307a56f211cd98
echoo35 <135036416+echoo35@users.noreply.github.com>
2024-04-23 18:09:40 +0300

    Add files via upload

---
+++ candy.js
@@ -0,0 +1,195 @@
+var candies = ["Blue", "Orange", "Green", "Yellow", "Red", "Purple"]; // Oyun tahtasında kullanılacak şekerlerin türlerini içeren dizi.
+var board = []; // Oyun tahtasını temsil eden boş bir dizi. Oyun sırasında şekerlerin yerleştirileceği ve hareket edeceği alan.
+var rows = 9; // Oyun tahtasındaki satır sayısı.
+var columns = 9; // Oyun tahtasındaki sütun sayısı.
+var score = 0; // Oyuncunun topladığı puanı tutan değişken.
+
+var currTile;
+var otherTile; // Eşleşme kontrolü yapmak için kullanılır.
+
+window.onload = function() {
+    startGame();
+
+    // Saniyenin 1/10'u
+    window.setInterval(function(){
+        crushCandy();
+        slideCandy();
+        generateCandy();
+    }, 100);
+}
+
+function randomCandy() {
+    return candies[Math.floor(Math.random() * candies.length)]; // Rasgele bir şeker türü seçer.
+}
+
+function startGame() {
+    for (let r = 0; r < rows; r++) {
+        let row = [];
+        for (let c = 0; c < columns; c++) {
+            let tile = document.createElement("img");
+            tile.id = r.toString() + "-" + c.toString();
+            tile.src = "./images/" + randomCandy() + ".png";
+
+            // Sürükle ve bırak işlemleri için olay dinleyicilerini ekler.
+            tile.addEventListener("dragstart", dragStart); // Bir şekere tıklandığında sürükleme işlemini başlatır.
+            tile.addEventListener("dragover", dragOver);  // Şekeri sürüklerken fare konumunu yakalar.
+            tile.addEventListener("dragenter", dragEnter); // Şekeri başka bir şekerin üzerine sürüklerken tetiklenir.
+            tile.addEventListener("dragleave", dragLeave); // Şekeri bırakırken tetiklenir.
+            tile.addEventListener("drop", dragDrop); // Bir şekeri başka bir şekerin üzerine bırakırken tetiklenir.
+            tile.addEventListener("dragend", dragEnd); // Sürükleme işlemi tamamlandıktan sonra tetiklenir.
+
+            document.getElementById("board").append(tile);
+            row.push(tile);
+        }
+        board.push(row);
+    }
+
+    console.log(board);
+}
+
+function dragStart() {
+    // Sürüklemek için tıklanan kare şekerini belirler.
+    currTile = this;
+}
+
+function dragOver(e) {
+    e.preventDefault();
+}
+
+function dragEnter(e) {
+    e.preventDefault();
+}
+
+function dragLeave() {
+
+}
+
+function dragDrop() {
+    // Üzerine bırakılan hedef döşemeyi belirler.
+    otherTile = this;
+}
+
+function dragEnd() {
+    if (currTile.src.includes("blank") || otherTile.src.includes("blank")) {
+        return;
+    }
+
+    let currCoords = currTile.id.split("-");
+    let r = parseInt(currCoords[0]);
+    let c = parseInt(currCoords[1]);
+
+    let otherCoords = otherTile.id.split("-");
+    let r2 = parseInt(otherCoords[0]);
+    let c2 = parseInt(otherCoords[1]);
+
+    let moveLeft = c2 == c-1 && r == r2;
+    let moveRight = c2 == c+1 && r == r2;
+    let moveUp = r2 == r-1 && c == c2;
+    let moveDown = r2 == r+1 && c == c2;
+
+    let isAdjacent = moveLeft || moveRight || moveUp || moveDown;
+
+    if (isAdjacent) {
+        let currImg = currTile.src;
+        let otherImg = otherTile.src;
+        currTile.src = otherImg;
+        otherTile.src = currImg;
+
+        let validMove = checkValid();
+        if (!validMove) {
+            let currImg = currTile.src;
+            let otherImg = otherTile.src;
+            currTile.src = otherImg;
+            otherTile.src = currImg;    
+        }
+    }
+}
+
+function crushCandy() {
+    crushThree(); // Üçlü eşleşmeleri kontrol eder.
+    document.getElementById("score").innerText = score; // Skoru günceller.
+}
+
+function crushThree() {
+    // Satırları kontrol eder.
+    for (let r = 0; r < rows; r++) {
+        for (let c = 0; c < columns-2; c++) {
+            let candy1 = board[r][c];
+            let candy2 = board[r][c+1];
+            let candy3 = board[r][c+2];
+            if (candy1.src == candy2.src && candy2.src == candy3.src && !candy1.src.includes("blank")) {
+                candy1.src = "./images/blank.png";
+                candy2.src = "./images/blank.png";
+                candy3.src = "./images/blank.png";
+                score += 30;
+            }
+        }
+    }
+
+    // Sütunları kontrol eder.
+    for (let c = 0; c < columns; c++) {
+        for (let r = 0; r < rows-2; r++) {
+            let candy1 = board[r][c];
+            let candy2 = board[r+1][c];
+            let candy3 = board[r+2][c];
+            if (candy1.src == candy2.src && candy2.src == candy3.src && !candy1.src.includes("blank")) {
+                candy1.src = "./images/blank.png";
+                candy2.src = "./images/blank.png";
+                candy3.src = "./images/blank.png";
+                score += 30;
+            }
+        }
+    }
+}
+
+function checkValid() {
+    // Satırları kontrol eder.
+    for (let r = 0; r < rows; r++) {
+        for (let c = 0; c < columns-2; c++) {
+            let candy1 = board[r][c];
+            let candy2 = board[r][c+1];
+            let candy3 = board[r][c+2];
+            if (candy1.src == candy2.src && candy2.src == candy3.src && !candy1.src.includes("blank")) {
+                return true;
+            }
+        }
+    }
+
+    // Sütunları kontrol eder.
+    for (let c = 0; c < columns; c++) {
+        for (let r = 0; r < rows-2; r++) {
+            let candy1 = board[r][c];
+            let candy2 = board[r+1][c];
+            let candy3 = board[r+2][c];
+            if (candy1.src == candy2.src && candy2.src == candy3.src && !candy1.src.includes("blank")) {
+                return true;
+            }
+        }
+    }
+
+    return false;
+}
+
+function slideCandy() {
+    for (let c = 0; c < columns; c++) {
+        let ind = rows - 1;
+        for (let r = columns-1; r >= 0; r--) {
+            if (!board[r][c].src.includes("blank")) {
+                board[ind][c].src = board[r][c].src;
+                ind -= 1;
+            }
+        }
+
+        for (let r = ind; r >= 0; r--) {
+            board[r][c].src = "./images/blank.png";
+        }
+    }
+}
+
+function generateCandy() {
+    for (let c = 0; c < columns;  c++) {
+        if (board[0][c].src.includes("blank")) {
+            board[0][c].src = "./images/" + randomCandy() + ".png";
+        }
+    }
+}
